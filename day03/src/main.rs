42e62f3c5ac70c903905f6cd12e997e7a24bb22b
qucker135 <qucker135@gmail.com>
2023-12-03 15:59:42 +0100

    Day03 - **

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -1,6 +1,5 @@
 use std::fs::File;
 use std::io::{self, prelude::*, BufReader};
-use regex::Regex;
 
 fn main() -> io::Result<()> {
     let file = File::open("src/input.txt")?;
@@ -16,70 +15,111 @@ fn main() -> io::Result<()> {
         }
     }
 
-    let re = Regex::new(r"[0-9]+").unwrap();
-
-    for (line_nr, line) in scheme.iter().enumerate() {
-        for m in re.find_iter(line) {
-            let mut to_add = false;
-
-            let start = m.start();
-            let end = m.end();
-
-            // ABOVE
-            if line_nr > 0 {
-                for i in start..end {
-                    if scheme[line_nr - 1].chars().nth(i) != Some('.') && scheme[line_nr - 1].chars().nth(i).unwrap().to_digit(10).is_none() {
-                        to_add = true;
+    for (line_nr, _line) in scheme.iter().enumerate() {
+        for (sym_nr, sym) in scheme[line_nr].chars().enumerate() {
+            if sym == '*' {
+                // println!("{}", sym_nr);
+                let mut neigh_nums :Vec<i32> = vec![];
+                
+                // LEFT
+                if sym_nr > 0 && scheme[line_nr].chars().nth(sym_nr - 1).unwrap().is_ascii_digit() {
+                    let mut start = sym_nr - 1;
+                    while start >= 1 && scheme[line_nr].chars().nth(start - 1).unwrap().is_ascii_digit() {
+                        start -= 1;
                     }
+                    neigh_nums.push((&scheme[line_nr][start..sym_nr]).parse::<i32>().unwrap());
                 }
-            }
-            // BELOW
-            if line_nr + 1 < scheme.len() {
-                for i in start..end {
-                    if scheme[line_nr + 1].chars().nth(i) != Some('.') && scheme[line_nr + 1].chars().nth(i).unwrap().to_digit(10).is_none() {
-                        to_add = true;
-                    }
-                }
-            }
-            // LEFT
-            if m.start() > 0 {
-                if scheme[line_nr].chars().nth(start - 1) != Some('.') {
-                    to_add = true;
-                }
-                if line_nr > 0 {
-                    if scheme[line_nr - 1].chars().nth(start - 1) != Some('.') && scheme[line_nr - 1].chars().nth(start - 1).unwrap().to_digit(10).is_none() {
-                        to_add = true;
-                    }
-                }
-                if line_nr + 1 < scheme.len() {
-                    if scheme[line_nr + 1].chars().nth(start - 1) != Some('.') && scheme[line_nr + 1].chars().nth(start - 1).unwrap().to_digit(10).is_none() {
-                        to_add = true;
-                    }
-                }
-            }
 
-            // RIGHT
-            if m.end() + 1 < scheme[line_nr].len() {
-                if scheme[line_nr].chars().nth(end) != Some('.') {
-                    to_add = true;
+                // RIGHT
+                if sym_nr + 1 < scheme[line_nr].len() && scheme[line_nr].chars().nth(sym_nr + 1).unwrap().is_ascii_digit() {
+                    let mut end = sym_nr + 2;
+                    while end < scheme[line_nr].len() && scheme[line_nr].chars().nth(end).unwrap().is_ascii_digit() {
+                        end += 1;
+                    }
+                    neigh_nums.push((&scheme[line_nr][sym_nr+1..end]).parse::<i32>().unwrap());
                 }
+
+                // ABOVE
                 // assume all lines have equal length
                 if line_nr > 0 {
-                    if scheme[line_nr - 1].chars().nth(end) != Some('.') && scheme[line_nr - 1].chars().nth(end).unwrap().to_digit(10).is_none() {
-                        to_add = true;
+                    if scheme[line_nr - 1].chars().nth(sym_nr).unwrap().is_ascii_digit() {
+                        //find start and end
+                        let mut start = sym_nr;
+                        let mut end = sym_nr + 1;
+
+                        while start >= 1 && scheme[line_nr - 1].chars().nth(start - 1).unwrap().is_ascii_digit() {
+                            start -= 1;
+                        }
+
+                        while end < scheme[line_nr - 1].len() && scheme[line_nr - 1].chars().nth(end).unwrap().is_ascii_digit() {
+                            end += 1;
+                        }
+
+                        neigh_nums.push((&scheme[line_nr - 1][start..end]).parse::<i32>().unwrap());
                     }
-                }
+                    else {
+                        //algo similar to left & right cases, but for line above
+                        if sym_nr > 0 && scheme[line_nr - 1].chars().nth(sym_nr - 1).unwrap().is_ascii_digit() {
+                            let mut start = sym_nr - 1;
+                            while start >= 1 && scheme[line_nr - 1].chars().nth(start - 1).unwrap().is_ascii_digit() {
+                                start -= 1;
+                            }
+                            neigh_nums.push((&scheme[line_nr - 1][start..sym_nr]).parse::<i32>().unwrap());
+                        }
+                        
+                        if sym_nr + 1 < scheme[line_nr - 1].len() && scheme[line_nr - 1].chars().nth(sym_nr + 1).unwrap().is_ascii_digit() {
+                            let mut end = sym_nr + 2;
+                            while end < scheme[line_nr - 1].len() && scheme[line_nr - 1].chars().nth(end).unwrap().is_ascii_digit() {
+                                end += 1;
+                            }
+                            neigh_nums.push((&scheme[line_nr - 1][sym_nr+1..end]).parse::<i32>().unwrap());
+                        }
+                    }
+                } 
+
+
+                // BELOW
                 // as above
                 if line_nr + 1 < scheme.len() {
-                    if scheme[line_nr + 1].chars().nth(end) != Some('.') && scheme[line_nr + 1].chars().nth(end).unwrap().to_digit(10).is_none() {
-                        to_add = true;
+                     if scheme[line_nr + 1].chars().nth(sym_nr).unwrap().is_ascii_digit() {
+                        //find start and end
+                        let mut start = sym_nr;
+                        let mut end = sym_nr + 1;
+
+                        while start >= 1 && scheme[line_nr + 1].chars().nth(start - 1).unwrap().is_ascii_digit() {
+                            start -= 1;
+                        }
+
+                        while end < scheme[line_nr + 1].len() && scheme[line_nr + 1].chars().nth(end).unwrap().is_ascii_digit() {
+                            end += 1;
+                        }
+
+                        neigh_nums.push((&scheme[line_nr + 1][start..end]).parse::<i32>().unwrap());
                     }
+                    else {
+                        //algo similar to left & right cases, but for line below
+                        if sym_nr > 0 && scheme[line_nr + 1].chars().nth(sym_nr - 1).unwrap().is_ascii_digit() {
+                            let mut start = sym_nr - 1;
+                            while start >= 1 && scheme[line_nr + 1].chars().nth(start - 1).unwrap().is_ascii_digit() {
+                                start -= 1;
+                            }
+                            neigh_nums.push((&scheme[line_nr + 1][start..sym_nr]).parse::<i32>().unwrap());
+                        }
+                        
+                        if sym_nr + 1 < scheme[line_nr + 1].len() && scheme[line_nr + 1].chars().nth(sym_nr + 1).unwrap().is_ascii_digit() {
+                            let mut end = sym_nr + 2;
+                            while end < scheme[line_nr + 1].len() && scheme[line_nr + 1].chars().nth(end).unwrap().is_ascii_digit() {
+                                end += 1;
+                            }
+                            neigh_nums.push((&scheme[line_nr + 1][sym_nr+1..end]).parse::<i32>().unwrap());
+                        }
+                    }
+                   
                 }
 
-            }
-
-            if to_add {
-                result += m.as_str().parse::<i32>().unwrap();
+                if neigh_nums.len() == 2 {
+                    result += neigh_nums[0] * neigh_nums[1];
+                }
             }
         }
     }
