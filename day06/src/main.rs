35e9502e639fe6e5a1eb0fb2675278fd25213c04
qucker135 <qucker135@gmail.com>
2023-12-07 08:53:46 +0100

    Day06 - **

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -1,7 +1,6 @@
 use std::fs::File;
 use std::io::{self, prelude::*, BufReader};
 use regex::Regex;
-use std::iter::zip;
 
 fn main() -> io::Result<()> {
     let file = File::open("src/input.txt")?;
@@ -9,32 +8,31 @@ fn main() -> io::Result<()> {
 
     let re = Regex::new(r"[0-9]+").unwrap();
 
-    let mut result = 1;
+    let mut result = 0;
 
-    let mut times :Vec<i32> = vec![];
-    let mut distances :Vec<i32> = vec![];
+    let mut time :String = "".to_string();
+    let mut distance :String = "".to_string();
 
-    for (line_nr, line) in reader.lines().enumerate() {
+    for line in reader.lines() {
         if let Ok(content) = line {
             for m in re.find_iter(&content) {
                 if content.starts_with("Time:") {
-                    times.push(m.as_str().parse::<i32>().unwrap());
+                    time.push_str(m.as_str());
                 }
                 else if content.starts_with("Distance:") {
-                    distances.push(m.as_str().parse::<i32>().unwrap());
+                    distance.push_str(m.as_str());
                 }
             }
         }
     }
 
-    for (t, d) in zip(times.iter(), distances.iter()) {
-        let mut sols = 0;
-        for i in 0..t+1 {
-            if i * (t - i) > *d {
-                sols += 1;
-            }
+    let time :i64 = time.parse::<i64>().unwrap();
+    let distance :i64 = distance.parse::<i64>().unwrap();
+
+    for i in 0..time+1 {
+        if i * (time - i) > distance {
+            result += 1;
         }
-        result *= sols;
     }
 
     println!("Final answer: {}", result);
