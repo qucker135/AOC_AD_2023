aafffce82563450e62d4bcd068d711ca2a50a123
qucker135 <qucker135@gmail.com>
2023-12-08 18:10:19 +0100

    Day08 - **

--- day08/src/main.rs
+++ day08/src/main.rs
@@ -1,46 +1,64 @@
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{self, prelude::*, BufReader};
-use std::collections::HashMap;
+
+fn gcd(a: i64, b: i64) -> i64 {
+    if b == 0 {
+        return a;
+    }
+    gcd(b, a % b)
+}
+
+fn lcm(a: i64, b: i64) -> i64 {
+    a / gcd(a, b) * b
+}
 
 fn main() -> io::Result<()> {
     let file = File::open("src/input.txt")?;
     let reader = BufReader::new(file);
 
-    let mut result = 0;
-
-    let mut instructions :String = "".to_string();
-    let mut map :HashMap<String, (String, String)> = HashMap::new();
+    let mut instructions: String = "".to_string();
+    let mut map: HashMap<String, (String, String)> = HashMap::new();
 
     for (line_nr, line) in reader.lines().enumerate() {
         if let Ok(content) = line {
             if line_nr == 0 {
                 instructions = content.to_string();
-            }
-            else if line_nr > 1 {
-                let key :String = content[0..3].to_string();
-                let v1 :String = content[7..10].to_string();
-                let v2 :String = content[12..15].to_string();
+            } else if line_nr > 1 {
+                let key: String = content[0..3].to_string();
+                let v1: String = content[7..10].to_string();
+                let v2: String = content[12..15].to_string();
 
                 map.insert(key, (v1, v2));
             }
         }
     }
 
-    let mut cur_pos = "AAA".to_string();
+    let mut steps_for_pos: Vec<i64> = vec![];
 
-    while cur_pos != "ZZZ".to_string() {
-        let instr_len = instructions.len();
+    for pos in map.keys() {
+        if pos.chars().nth(2) == Some('A') {
+            let mut steps = 0;
 
-        if instructions.chars().nth(result % instr_len) == Some('L') {
-            cur_pos = map[&cur_pos].0.clone();
+            let mut cur_pos = pos.clone();
+
+            while cur_pos.chars().nth(2) != Some('Z') {
+                let instr_len = instructions.len();
+
+                if instructions.chars().nth(steps % instr_len) == Some('L') {
+                    cur_pos = map[&cur_pos].0.clone();
+                } else {
+                    cur_pos = map[&cur_pos].1.clone();
+                }
+
+                steps += 1;
+            }
+
+            steps_for_pos.push(steps as i64);
         }
-
-        else {
-            cur_pos = map[&cur_pos].1.clone();
-        }
-
-        result += 1;
     }
+
+    let result = steps_for_pos.into_iter().reduce(lcm).unwrap();
 
     println!("Final answer: {}", result);
 
