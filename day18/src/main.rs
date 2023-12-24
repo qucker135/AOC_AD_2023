3ed8e00194a4ead5171bc767420c3dc4794f7e62
qucker135 <qucker135@gmail.com>
2023-12-24 16:24:17 +0100

    Day18 - **

--- day18/src/main.rs
+++ day18/src/main.rs
@@ -1,114 +1,142 @@
-use std::cmp::{max, min};
+use std::cmp::max;
 use std::fs::File;
 use std::io::{self, prelude::*, BufReader};
+use std::iter::zip;
+
+#[derive(Copy, Clone, Eq, PartialEq, Debug)]
+enum Direction {
+    Left,
+    Right,
+    Up,
+    Down,
+}
+
+fn area(points: &[(i64, i64)]) -> i64 {
+    let mut res = 0i64;
+
+    for (p1, p2) in zip(
+        points.iter(),
+        points.iter().cycle().skip(1).take(points.len()),
+    ) {
+        res += p1.0 * p2.1 - p2.0 * p1.1;
+    }
+
+    res.abs() / 2
+}
+
+fn max_area(points1: &[(i64, i64)], points2: &[(i64, i64)]) -> i64 {
+    max(area(points1), area(points2))
+}
 
 fn main() -> io::Result<()> {
     let file = File::open("src/input.txt")?;
     let reader = BufReader::new(file);
 
-    let mut result: i64 = 0;
-
-    let mut plan: Vec<(String, i64, String)> = vec![];
-
-    let (mut y_min, mut y_max, mut x_min, mut x_max): (i64, i64, i64, i64) = (0, 0, 0, 0);
-
-    let (mut y_cur, mut x_cur): (i64, i64) = (0, 0);
+    let mut plan: Vec<(Direction, i64)> = vec![];
 
     for line in reader.lines().map_while(Result::ok) {
-        let vec: Vec<&str> = line.split(' ').collect();
-        let steps = vec[1].parse::<i64>().unwrap();
-        plan.push((vec[0].to_string(), steps, vec[2].to_string()));
-        if vec[0] == "U" {
-            y_cur -= steps;
-            y_min = min(y_min, y_cur);
-        } else if vec[0] == "D" {
-            y_cur += steps;
-            y_max = max(y_max, y_cur);
-        } else if vec[0] == "L" {
-            x_cur -= steps;
-            x_min = min(x_min, x_cur);
-        } else if vec[0] == "R" {
-            x_cur += steps;
-            x_max = max(x_max, x_cur);
+        let color: &str = &line.split(' ').nth(2).unwrap()[2..8];
+
+        let num = i64::from_str_radix(&color[..5], 16).unwrap();
+
+        match color.chars().last().unwrap() {
+            '0' => plan.push((Direction::Right, num)),
+            '1' => plan.push((Direction::Down, num)),
+            '2' => plan.push((Direction::Left, num)),
+            '3' => plan.push((Direction::Up, num)),
+            _ => {}
         }
     }
 
-    let mut lagoon: Vec<Vec<char>> = vec![];
+    type PointsList = Vec<(i64, i64)>;
 
-    for _ in 0..y_max - y_min + 1 {
-        lagoon.push(vec!['.'; (x_max - x_min + 1) as usize]);
-    }
+    let (mut points_left, mut points_right): (PointsList, PointsList) = (vec![], vec![]);
 
-    for com in plan.iter() {
-        if com.0 == "U" {
-            for _ in 1..=com.1 {
-                y_cur -= 1;
-                lagoon[(y_cur - y_min) as usize][(x_cur - x_min) as usize] = '#';
-            }
-        } else if com.0 == "D" {
-            for _ in 1..=com.1 {
-                y_cur += 1;
-                lagoon[(y_cur - y_min) as usize][(x_cur - x_min) as usize] = '#';
-            }
-        } else if com.0 == "L" {
-            for _ in 1..=com.1 {
-                x_cur -= 1;
-                lagoon[(y_cur - y_min) as usize][(x_cur - x_min) as usize] = '#';
-            }
-        } else if com.0 == "R" {
-            for _ in 1..=com.1 {
-                x_cur += 1;
-                lagoon[(y_cur - y_min) as usize][(x_cur - x_min) as usize] = '#';
-            }
-        }
-    }
+    let (mut cur_x_left, mut cur_y_left, mut cur_x_right, mut cur_y_right) =
+        (0i64, 0i64, 0i64, 0i64);
 
-    for row in &mut lagoon {
-        if row[0] != '#' {
-            row[0] = 'O';
-        }
-        if row.last() != Some(&'#') {
-            let ind = row.len() - 1;
-            row[ind] = 'O';
-        }
-    }
+    for i in 0..plan.len() {
+        let i_prev = (i + plan.len() - 1) % plan.len();
+        let i_next = (i + 1) % plan.len();
 
-    for x in 0..lagoon[0].len() {
-        if lagoon[0][x] != '#' {
-            lagoon[0][x] = 'O';
-        }
-        if lagoon.last().unwrap()[x] != '#' {
-            let ind = lagoon.len() - 1;
-            lagoon[ind][x] = 'O';
-        }
-    }
-
-    let mut change = true;
-
-    while change {
-        change = false;
-        for y in 1..lagoon.len() - 1 {
-            for x in 1..lagoon[y].len() - 1 {
-                if lagoon[y][x] == '.'
-                    && (lagoon[y - 1][x] == 'O'
-                        || lagoon[y + 1][x] == 'O'
-                        || lagoon[y][x - 1] == 'O'
-                        || lagoon[y][x + 1] == 'O')
-                {
-                    lagoon[y][x] = 'O';
-                    change = true;
+        if plan[i_prev].0 == plan[i_next].0 {
+            match plan[i].0 {
+                Direction::Left => {
+                    cur_x_left -= plan[i].1;
+                    cur_x_right -= plan[i].1;
+                }
+                Direction::Right => {
+                    cur_x_left += plan[i].1;
+                    cur_x_right += plan[i].1;
+                }
+                Direction::Up => {
+                    cur_y_left += plan[i].1;
+                    cur_y_right += plan[i].1;
+                }
+                Direction::Down => {
+                    cur_y_left -= plan[i].1;
+                    cur_y_right -= plan[i].1;
                 }
             }
-        }
-    }
-
-    for row in lagoon.iter() {
-        for ch in row.iter() {
-            if ch == &'#' || ch == &'.' {
-                result += 1;
+        } else {
+            // left inner
+            if plan[i_prev].0 == Direction::Down
+                && plan[i].0 == Direction::Right
+                && plan[i_next].0 == Direction::Up
+            {
+                cur_x_left += plan[i].1 - 1;
+                cur_x_right += plan[i].1 + 1;
+            } else if plan[i_prev].0 == Direction::Up
+                && plan[i].0 == Direction::Left
+                && plan[i_next].0 == Direction::Down
+            {
+                cur_x_left -= plan[i].1 - 1;
+                cur_x_right -= plan[i].1 + 1;
+            } else if plan[i_prev].0 == Direction::Right
+                && plan[i].0 == Direction::Up
+                && plan[i_next].0 == Direction::Left
+            {
+                cur_y_left += plan[i].1 - 1;
+                cur_y_right += plan[i].1 + 1;
+            } else if plan[i_prev].0 == Direction::Left
+                && plan[i].0 == Direction::Down
+                && plan[i_next].0 == Direction::Right
+            {
+                cur_y_left -= plan[i].1 - 1;
+                cur_y_right -= plan[i].1 + 1;
+            }
+            // right inner
+            else if plan[i_prev].0 == Direction::Down
+                && plan[i].0 == Direction::Left
+                && plan[i_next].0 == Direction::Up
+            {
+                cur_x_left -= plan[i].1 + 1;
+                cur_x_right -= plan[i].1 - 1;
+            } else if plan[i_prev].0 == Direction::Up
+                && plan[i].0 == Direction::Right
+                && plan[i_next].0 == Direction::Down
+            {
+                cur_x_left += plan[i].1 + 1;
+                cur_x_right += plan[i].1 - 1;
+            } else if plan[i_prev].0 == Direction::Right
+                && plan[i].0 == Direction::Down
+                && plan[i_next].0 == Direction::Left
+            {
+                cur_y_left -= plan[i].1 + 1;
+                cur_y_right -= plan[i].1 - 1;
+            } else if plan[i_prev].0 == Direction::Left
+                && plan[i].0 == Direction::Up
+                && plan[i_next].0 == Direction::Right
+            {
+                cur_y_left += plan[i].1 + 1;
+                cur_y_right += plan[i].1 - 1;
             }
         }
+        points_left.push((cur_x_left, cur_y_left));
+        points_right.push((cur_x_right, cur_y_right));
     }
+
+    let result: i64 = max_area(&points_left, &points_right);
 
     println!("Final answer: {:?}", result);
 
