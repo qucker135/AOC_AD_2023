b5a8af08a9a196765604d99c07afa9001911542f
qucker135 <qucker135@gmail.com>
2024-01-06 21:09:03 +0100

    Day24 - **

--- day24/src/main.rs
+++ day24/src/main.rs
@@ -1,8 +1,7 @@
+use ndarray::prelude::*;
+use ndarray_linalg::Solve;
 use std::fs::File;
 use std::io::{self, prelude::*, BufReader};
-
-const MAX: i128 = 400_000_000_000_000;
-const MIN: i128 = 200_000_000_000_000;
 
 #[derive(Debug, Clone, Copy, PartialEq)]
 struct Trajectory {
@@ -14,32 +13,88 @@ struct Trajectory {
     vz: i128,
 }
 
-fn check_collision(a: Trajectory, b: Trajectory) -> bool {
-    let w: i128 = b.vx * a.vy - a.vx * b.vy;
-    // check if the two trajectories are parallel
-    if w == 0 {
-        return false;
+fn get_coeffs_4_dim(a: Trajectory, b: Trajectory, iz: bool) -> ([i128; 4], i128) {
+    if !iz {
+        return (
+            [a.py - b.py, b.vy - a.vy, b.px - a.px, a.vx - b.vx],
+            b.vy * b.px - b.py * b.vx - a.vy * a.px + a.py * a.vx,
+        );
     }
-    let wa: i128 = b.vx * (b.py - a.py) - b.vy * (b.px - a.px);
-    let wb: i128 = a.vx * (b.py - a.py) - a.vy * (b.px - a.px);
+    (
+        [a.pz - b.pz, b.vz - a.vz, b.px - a.px, a.vx - b.vx],
+        b.vz * b.px - b.pz * b.vx - a.vz * a.px + a.pz * a.vx,
+    )
+}
 
-    // check if collision would happen in the future
-    if wa.signum() != w.signum() || wb.signum() != w.signum() {
-        return false;
+fn solve_vs_helper(trajectories: &[Trajectory], iz: bool) -> (i128, i128) {
+    let mut a: Vec<[i128; 4]> = vec![];
+    let mut b: Vec<i128> = vec![];
+
+    for i in 1..=4 {
+        let (a_row, b_scalar) =
+            get_coeffs_4_dim(trajectories[2 * i - 2], trajectories[2 * i - 1], iz);
+        a.push(a_row);
+        b.push(b_scalar);
     }
 
-    // check if collision would happen in considered area
-    w.abs() * (MIN - a.px) <= a.vx * wa.abs()
-        && a.vx * wa.abs() <= w.abs() * (MAX - a.px)
-        && w.abs() * (MIN - a.py) <= a.vy * wa.abs()
-        && a.vy * wa.abs() <= w.abs() * (MAX - a.py)
+    let a_f: Vec<[f64; 4]> = a
+        .iter()
+        .map(|x| [x[0] as f64, x[1] as f64, x[2] as f64, x[3] as f64])
+        .collect();
+    let b_f: Vec<f64> = b.iter().map(|x| *x as f64).collect();
+
+    let a_r: Array2<f64> = array![a_f[0], a_f[1], a_f[2], a_f[3]];
+    let b_r: Array1<f64> = array![b_f[0], b_f[1], b_f[2], b_f[3]];
+
+    let x = a_r.solve_into(b_r).unwrap();
+
+    let x = x.iter().map(|x| x.round() as i128).collect::<Vec<_>>();
+
+    (x[0], x[2])
+}
+
+fn solve_ps_helper(
+    trajectories: &[Trajectory],
+    (vx, vy, vz): (i128, i128, i128),
+) -> (i128, i128, i128) {
+    let a = [
+        [
+            vx - trajectories[5].vx,
+            trajectories[5].vy - vy,
+            trajectories[5].vz - vz,
+        ],
+        [
+            vx - trajectories[6].vx,
+            trajectories[6].vy - vy,
+            trajectories[6].vz - vz,
+        ],
+    ];
+    let b = [
+        trajectories[5].px * (trajectories[5].vy - vy)
+            + trajectories[5].py * (vx - trajectories[5].vx),
+        trajectories[6].px * (trajectories[6].vy - vy)
+            + trajectories[6].py * (vx - trajectories[6].vx),
+        trajectories[5].px * (trajectories[5].vz - vz)
+            + trajectories[5].pz * (vx - trajectories[5].vx),
+        trajectories[6].px * (trajectories[6].vz - vz)
+            + trajectories[6].pz * (vx - trajectories[6].vx),
+    ];
+    let wxy = a[0][0] * a[1][1] - a[0][1] * a[1][0];
+    let wx = b[0] * a[1][1] - b[1] * a[0][1];
+    let wy = a[0][0] * b[1] - a[1][0] * b[0];
+
+    let wxz = a[0][0] * a[1][2] - a[0][2] * a[1][0];
+    let wz = b[2] * a[1][2] - b[3] * a[0][2];
+
+    let x = wx / wxy;
+    let y = wy / wxy;
+    let z = wz / wxz;
+    (x, y, z)
 }
 
 fn main() -> io::Result<()> {
     let file = File::open("src/input.txt")?;
     let reader = BufReader::new(file);
-
-    let mut result: i128 = 0;
 
     let mut trajectories: Vec<Trajectory> = vec![];
 
@@ -59,13 +114,16 @@ fn main() -> io::Result<()> {
         });
     }
 
-    for i in 0..trajectories.len() {
-        for j in i + 1..trajectories.len() {
-            if check_collision(trajectories[i], trajectories[j]) {
-                result += 1;
-            }
-        }
-    }
+    let (_, vy) = solve_vs_helper(&trajectories, false);
+    let (vx, vz) = solve_vs_helper(&trajectories, true);
+
+    println!("vx = {}, vy = {}, vz = {}", vx, vy, vz);
+
+    let (px, py, pz) = solve_ps_helper(&trajectories, (vx, vy, vz));
+
+    println!("px = {}, py = {}, pz = {}", px, py, pz);
+
+    let result = px + py + pz;
 
     println!("Final answer: {:?}", result);
 
